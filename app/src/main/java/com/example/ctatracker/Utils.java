2500f2588e54e0a33e5d5a2ccebbd9114b162a18
DrewSnarski <asnarski1@hawk.iit.edu>
2022-07-21 14:41:37 -0500

    Add unit test for empty username

--- app/src/main/java/com/example/ctatracker/Utils.java
+++ app/src/main/java/com/example/ctatracker/Utils.java
@@ -7,6 +7,11 @@ public class Utils {
         Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
         return matcher.find();
     }
+    public static boolean checkUsernameEmpty(String uname) {
+        if(uname == "") return false;
+        else return true;
+    }
+
     private static final Pattern VALID_EMAIL_ADDRESS_REGEX =
             Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
