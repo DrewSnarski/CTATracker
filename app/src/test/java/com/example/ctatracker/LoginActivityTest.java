920678138146f8da26edfa738343e940f60d216e
Drew Snarski <70114526+DrewSnarski@users.noreply.github.com>
2022-07-21 14:20:53 -0500

    Update LoginActivityTest.java

--- app/src/test/java/com/example/ctatracker/LoginActivityTest.java
+++ app/src/test/java/com/example/ctatracker/LoginActivityTest.java
@@ -9,7 +9,7 @@ public class LoginActivityTest {
 
     @Test
     public void testIsEmailValid() {
-        String testEmail = " asnarski1@hawk.iit.edu";
+        String testEmail = "asnarski1@hawk.iit.edu";
         Assert.assertThat(String.format("Email Validity Test failed for %s ", testEmail), Utils.checkEmailForValidity(testEmail), is(true));
     }
 }
